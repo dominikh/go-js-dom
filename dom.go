dominikh/go-js-dom

commit a0c5c87543a22db04a5b401b237d2ee7d98473d1

attributes for HTMLScriptElement

dom.go
@@ -1808,7 +1808,16 @@ type HTMLQuoteElement struct {
 	Cite string `js:"cite"`
 }
 
-type HTMLScriptElement struct{ *BasicHTMLElement }
+type HTMLScriptElement struct {
+	*BasicHTMLElement
+	Type    string `js:"type"`
+	Src     string `js:"src"`
+	Charset string `js:"charset"`
+	Async   bool   `js:"async"`
+	Defer   bool   `js:"defer"`
+	Text    string `js:"text"`
+}
+
 type HTMLSelectElement struct{ *BasicHTMLElement }
 
 type HTMLSourceElement struct {

commit 4526980b8b0b6d6d6595202f5d37737dc5ff4085

fix build (regression in a0c5c87)

dom.go
@@ -241,7 +241,7 @@ func wrapHTMLElement(o js.Object) HTMLElement {
 	case "HTMLQuoteElement":
 		return &HTMLQuoteElement{BasicHTMLElement: el}
 	case "HTMLScriptElement":
-		return &HTMLScriptElement{el}
+		return &HTMLScriptElement{BasicHTMLElement: el}
 	case "HTMLSelectElement":
 		return &HTMLSelectElement{el}
 	case "HTMLSourceElement":